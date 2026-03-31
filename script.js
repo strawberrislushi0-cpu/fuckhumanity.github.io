d2ae52fb629b7d84edce48de77bde3aa7800f506
strawberrislushi0-cpu <strawberrislushi0@gmail.com>
2026-03-31 09:14:45 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const title = document.getElementById("title");
+import { Client, Account, Databases, ID } from "https://cdn.jsdelivr.net/npm/appwrite@13.0.0/+esm";
+
+const client = new Client()
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject("YOUR_PROJECT_ID");
+
+const account = new Account(client);
+const databases = new Databases(client);
